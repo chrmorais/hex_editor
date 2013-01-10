b68c1b044d08afaa1a236a7bc8fb0d8c223a0a19
Dimercel <xolcman@gmail.com>
2013-01-10 21:44:41 +0400

    test commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ncurses.h>
+
+using namespace std;
+
+int main( int argc, const char* argv[] ){
+  initscr();
+
+
+  start_color();
+  //bkgdset(COLOR_PAIR(1));
+  int maxx = getmaxx(stdscr);
+  int maxy = getmaxy(stdscr);
+  
+  init_pair(1, COLOR_YELLOW, COLOR_BLUE);
+  attron(A_BOLD);
+  attron(COLOR_PAIR(1));
+  
+
+  move(0,0);
+  printw("%08X",0); // вывод строки
+  move(1,0);
+  printw("%08X",15); // вывод строки
+  move(2,0);
+  printw("%08X",31); // вывод строки
+  move(3,0);
+  printw("%08X",47); // вывод строки
+  move(4,0);
+  printw("%08X",63); // вывод строки
+  
+  
+  
+  //addchnstr("hello",-1);
+
+  refresh(); // обновить
+  
+  getch(); // ждём нажатия символа
+  
+  endwin(); // завершение работы с ncurses  
+}
