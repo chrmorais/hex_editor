14ff729acd742ef8be323fb51ca43d324164e60d
Dimercel <xolcman@gmail.com>
2015-06-02 14:29:26 +0300

    Application class

--- application.py
+++ application.py
@@ -11,10 +11,21 @@ import file_buffer
 
 class Application:
     def __init__(self, context, file_path):
-        pass
+        self.__file_size = os.stat(file_path).st_size
+
+        file_buffer.init(128)
+        file_buffer.open_file(file_path)
+
+        self.__context = context
+
+        self.__init_context(self.__context)
+        self.__max_y, self.__max_x = self.__context.getmaxyx()
+
+        self.__init_GUI()
+
 
     def __init_GUI(self):
-        self.address_block = AddressBlock(Rect(0,0,8,max_y))
+        self.address_block = AddressBlock(Rect(0,0,8,self.__max_y))
         self.address_block.start_address = 0
         self.address_block.color = 2
         self.address_block.highlight = 3
@@ -22,16 +33,89 @@ class Application:
 
         self.byte_grid = ByteGrid(Rect(self.address_block.draw_zone.width + 1,
                                        0,
-                                       max_x - self.address_block.draw_zone.width - 1,
-                                       max_y))
+                                       self.__max_x - self.address_block.draw_zone.width - 1,
+                                       self.__max_y))
         self.byte_grid.color = 2
         self.byte_grid.highlight = 3
         self.byte_grid.data = bytearray()
         self.byte_grid.cursor_position = (1, 1)
+        self.byte_grid.autosize()
 
         self.address_block.step = self.byte_grid.col_count
 
     def __init_context(self, context):
-        pass
 
+        if curses.has_colors():
+            curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLUE)
+            curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
+            curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
 
+        context.bkgdset(curses.color_pair(2))
+
+    def keyloop(self):
+
+        data = []
+        data_size = self.byte_grid.col_count * self.byte_grid.row_count
+        if data_size > self.__file_size:
+            data_size = self.__file_size
+
+        for i in range(data_size):
+            data.append(ord(file_buffer.get_byte(i)))
+
+        self.byte_grid.data = bytearray(data)
+        self.__context.clear()
+
+        key = ''
+        while key != ord('q'):
+
+            self.address_block.draw(self.__context)
+            self.byte_grid.draw(self.__context)
+            self.__context.refresh()
+
+            key = self.__context.getch()
+            if key == curses.KEY_DOWN:
+                self.byte_grid.cursor_position = \
+                    (self.byte_grid.cursor_position[0]+1,
+                     self.byte_grid.cursor_position[1])
+                self.address_block.highlight_inx = \
+                    self.byte_grid.cursor_position[0]
+            if key == curses.KEY_UP:
+                self.byte_grid.cursor_position = \
+                    (self.byte_grid.cursor_position[0]-1,
+                    self.byte_grid.cursor_position[1])
+                self.address_block.highlight_inx = \
+                    self.byte_grid.cursor_position[0]
+            if key == curses.KEY_LEFT:
+                self.byte_grid.cursor_position = \
+                   (self.byte_grid.cursor_position[0],
+                    self.byte_grid.cursor_position[1]-1)
+                self.address_block.highlight_inx = \
+                    self.byte_grid.cursor_position[0]
+            if key == curses.KEY_RIGHT:
+                self.byte_grid.cursor_position = \
+                    (self.byte_grid.cursor_position[0],
+                    self.byte_grid.cursor_position[1]+1)
+                self.address_block.highlight_inx = \
+                    self.byte_grid.cursor_position[0]
+
+        self.exit()
+
+    def exit(self):
+        file_buffer.close_file()
+        curses.endwin()
+
+    @property
+    def address_block(self):
+        return self.__address_block
+
+    @address_block.setter
+    def address_block(self, widget):
+        self.__address_block = widget
+
+    @property
+    def byte_grid(self):
+        return self.__byte_grid
+
+    @byte_grid.setter
+    def byte_grid(self, widget):
+        self.__byte_grid = widget
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from GUI.address_block import *
 from GUI.byte_grid import *
 from GUI.ui_utils import Rectangle as Rect
+from application import Application
 
 import file_buffer
 
@@ -20,74 +21,9 @@ if not(os.path.isfile(file_path)):
 
 file_size = os.stat(file_path).st_size
 
-def keyloop(context):
-
-    max_y, max_x = context.getmaxyx()
-
-    address_block = AddressBlock(Rect(0,0,8,max_y))
-    address_block.start_address = 0
-    address_block.color = 2
-    address_block.highlight = 3
-    address_block.highlight_inx = 0
-
-    byte_grid = ByteGrid(Rect(address_block.draw_zone.width + 1,
-                              0,
-                              max_x - address_block.draw_zone.width - 1,
-                              max_y))
-    byte_grid.color = 2
-    byte_grid.highlight = 3
-    byte_grid.data = bytearray()
-    byte_grid.cursor_position = (1, 1)
-
-    address_block.step = byte_grid.col_count
-
-    file_buffer.init(128)
-    file_buffer.open_file(file_path)
-
-    data = []
-    data_size = byte_grid.col_count * byte_grid.row_count
-    if data_size > file_size:
-        data_size = file_size
-    for i in range(data_size):
-        data.append(ord(file_buffer.get_byte(i)))
-
-    byte_grid.data = bytearray(data)
-
-
-    if curses.has_colors():
-        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLUE)
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
-        curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
-
-        context.bkgdset(curses.color_pair(2))
-
-    context.clear()
-
-    key = ''
-    while key != ord('q'):
-
-        address_block.draw(context)
-        byte_grid.draw(context)
-        context.refresh()
-
-        key = context.getch()
-        if key == curses.KEY_DOWN:
-            byte_grid.cursor_position = (byte_grid.cursor_position[0]+1, byte_grid.cursor_position[1])
-            address_block.highlight_inx = byte_grid.cursor_position[0]
-        if key == curses.KEY_UP:
-            byte_grid.cursor_position = (byte_grid.cursor_position[0]-1, byte_grid.cursor_position[1])
-            address_block.highlight_inx = byte_grid.cursor_position[0]
-        if key == curses.KEY_LEFT:
-            byte_grid.cursor_position = (byte_grid.cursor_position[0], byte_grid.cursor_position[1]-1)
-            address_block.highlight_inx = byte_grid.cursor_position[0]
-        if key == curses.KEY_RIGHT:
-            byte_grid.cursor_position = (byte_grid.cursor_position[0], byte_grid.cursor_position[1]+1)
-            address_block.highlight_inx = byte_grid.cursor_position[0]
-
-    curses.endwin()
-
 def main(stdscr):
-    keyloop(stdscr)
+    app = Application(stdscr,file_path)
+    app.keyloop()
 
 if __name__ == '__main__':
     curses.wrapper(main)
