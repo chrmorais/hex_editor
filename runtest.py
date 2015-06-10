0a41cda7d135674b2b5f3ce56cae0859215f39d1
Dimercel <xolcman@gmail.com>
2015-06-10 19:15:06 +0300

    Add test for TextView

--- runtest.py
+++ runtest.py
@@ -2,8 +2,10 @@ import unittest
 
 from tests.test_address_block import TestAddressBlock
 from tests.test_byte_grid import TestByteGrid
+from tests.test_text_view import TestTextView
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestAddressBlock)
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestByteGrid))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestTextView))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ tests/test_text_view.py
@@ -0,0 +1,24 @@
+import unittest
+
+from GUI.text_view import TextView
+from GUI.ui_utils import Rectangle as Rect
+
+class TestTextView(unittest.TestCase):
+
+    def setUp(self):
+        self.model = TextView(Rect(0,0,10,10))
+
+    def test_set_not_correct_invisible_symbol(self):
+        self.model.invisible_symbol = ord('.')
+        self.model.invisible_symbol = 0
+
+        self.assertTrue(self.model.invisible_symbol != 0)
+
+    def test_set_symbol_in_row_more_than_width(self):
+        self.model.symbol_in_row = -1
+        self.model.symbol_in_row = self.model.draw_zone.width + 1
+
+        self.assertTrue(0 < self.model.symbol_in_row <= self.model.draw_zone.width)
+
+    def tearDown(self):
+        pass
